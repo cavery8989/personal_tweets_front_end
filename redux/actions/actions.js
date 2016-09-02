a653450e00548b9babcee46afddcc8e272fbaecb
cavery8989 <c.avery1989@gmail.com>
2016-09-02 13:01:48 +0100

    implemented action creators for redux

---
+++ redux/actions/actions.js
@@ -0,0 +1,46 @@
+//add profile 1,
+//add profile 2,
+//turn on loading
+//turn off loading;
+
+const types = require('./types')
+
+const actions = {};
+
+actions.loadingOn =  function () {
+  var obj = {};
+  obj.type = types.LOADING_ON;
+  return obj;
+};
+
+actions.loadingOff =  function () {
+  var obj = {};
+  obj.type = types.LOADING_OFF;
+  return obj;
+};
+
+actions.setProfileOne = function (profile) {
+  var obj = {};
+  obj.type = types.SET_PROFILE_ONE;
+  obj.profile = profile;
+  return obj;
+};
+
+actions.removeProfileOne = function () {
+  var obj = {};
+  obj.type = types.REMOVE_PROFILE_ONE;
+  return obj;
+};
+
+actions.setProfileTwo = function (profile) {
+  var obj= {};
+  obj.type = types.SET_PROFILE_TWO;
+  obj.profile= profile;
+  return obj;
+};
+
+actions.removeProfileTwo = function () {
+  var obj = {};
+  obj.type = types.REMOVE_PROFILE_TWO;
+  return obj;
+};
